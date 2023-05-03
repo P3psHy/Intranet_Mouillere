925a1950dc11e6cbbc62ad301e84129a8e0881e6
Martin Chanudet <machanudet@stpbb.org>
2023-05-03 11:42:57 +0200

    ajout css

--- BackOffice/BackVehicule/listeVehicule.php
+++ BackOffice/BackVehicule/listeVehicule.php
@@ -4,36 +4,41 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../css/styleForm.css">
+
     <title>Liste Utilisateurs</title>
 </head>
 <body>
-    <h2>Liste des Véhicules</h2>
-    <?php
-
-    require_once "../../php/connection.php";
-
-
-    $sqlVehicule=$connection ->prepare('SELECT * FROM vehicule;');
-
-    $sqlVehicule->execute();
-
-    $ligneVehicule = $sqlVehicule->fetchall();
-    foreach($ligneVehicule as $vehicule){
-        ?>
-        <div style="margin-bottom:10px;">
-            <label>Voiture: <?php echo $vehicule['marque']." ".$vehicule['modele']; ?></label>
-            <a href="modifierForm.php?id=<?php echo $vehicule['idVehicule'] ?>"><button>Modifier</button></a>
-            <a href="supprimer.php?id=<?php echo $vehicule['idVehicule'] ?>"><button>Supprimer</button></a>
-
-        </div>
+    <div class="box">
+        <h2 class="center">Liste des Véhicules</h2>
         <?php
-    }
+
+        require_once "../../php/connection.php";
+
+
+        $sqlVehicule=$connection ->prepare('SELECT * FROM vehicule;');
+
+        $sqlVehicule->execute();
+
+        $ligneVehicule = $sqlVehicule->fetchall();
+        foreach($ligneVehicule as $vehicule){
+            ?>
+            <div style="margin-bottom:10px;">
+                <label>Voiture: <?php echo $vehicule['marque']." ".$vehicule['modele']; ?></label>
+                <a href="modifierForm.php?id=<?php echo $vehicule['idVehicule'] ?>"><button>Modifier</button></a>
+                <a href="supprimer.php?id=<?php echo $vehicule['idVehicule'] ?>"><button>Supprimer</button></a>
+
+            </div>
+            <?php
+        }
 
 
 
 
-    ?>
-    <a href="ajouterForm.php"><button>Ajouter un utilisateur</button></a>
-    <a href="../BackOffice.php"><button>Retour</button></a>
+        ?>
+        <a href="ajouterForm.php"><button>Ajouter un utilisateur</button></a>
+        <a href="../BackOffice.php"><button>Retour</button></a>
+    </div>
+    
 </body>
 </html>
